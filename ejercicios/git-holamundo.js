47ec8562bb78a5a2c74f7d344be487b200cfe700
Laura Estefania Grajeda <Lauragrajedacardiel@gmail.com>
2024-10-17 19:44:24 -0600

    :sparkles:agregando la función del saludo encoreano

--- ejercicios/git-holamundo.js
+++ ejercicios/git-holamundo.js
@@ -5,6 +5,10 @@ function holaMundo(nombre) {
 function helloWorld(name) {
     console.log('Hello ' + name);
 }
+function 안녕하세요(name){
+    console.log('안녕하세요'+ name);
+}
 
 holaMundo('Alberto');
 helloWorld('Zaira');
+안녕하세요('Laura')
